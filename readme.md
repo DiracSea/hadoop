8c81419a341a54aa65593f158db24fe8e5510ae5
DiracSea <sulz@mail.sustc.edu.cn>
2019-10-18 23:06:37 -0700

    local

---
+++ readme.md
@@ -0,0 +1,42 @@
+<!--
+ * @Author: Longze Su
+ * @Date: 2019-10-18 03:41:58
+ * @Description: CS211_Project1
+ * @LastEditTime: 2019-10-18 23:04:12
+ * @LastEditors: Longze Su
+ -->
+# Assignment1 
+
+## Class
+
+### HDFSUpload 
+#### void write2HDFS(String src, String dst)
+write from file to HDFS 
+
+write from file to LocalFileSystem
+
+#### void readfromHDFS(String dst)
+read file from HDFS 
+
+read file from LocalFileSystem
+
+#### void randomAccess(String dst)
+random access from/to HDFS 
+
+random access from/to LocalFileSystem
+
+### Run
+#### main
+output
+
+## Results
+### HDFS
+Total copy time 38389ms
+
+Total read time 34985ms
+
+Total random access time 58931ms
+
+### Local
+
+
--- src/main/java/run.java
+++ src/main/java/run.java
@@ -2,7 +2,7 @@
  * @Author: Longze Su
  * @Date: 2019-10-18 09:49:25
  * @Description: CS211_Project1
- * @LastEditTime: 2019-10-18 22:44:00
+ * @LastEditTime: 2019-10-18 23:06:11
  * @LastEditors: Longze Su
  */
 // Longze.Su
@@ -27,20 +27,20 @@ public class run {
         file2HDFS f = new file2HDFS();
         long t1, t2, t3, t4, t5, t6;
         try {   
-            long start=System.currentTimeMillis();
-            f.write2HDFS(src, dst);
-            long end=System.currentTimeMillis();
-            t1 = end - start;
+            // long start=System.currentTimeMillis();
+            // f.write2HDFS(src, dst);
+            // long end=System.currentTimeMillis();
+            // t1 = end - start;
 
-            long start1=System.currentTimeMillis();
-            f.readfromHDFS(dst);
-            long end1=System.currentTimeMillis();
-            t2 = end1 - start1;
+            // long start1=System.currentTimeMillis();
+            // f.readfromHDFS(dst);
+            // long end1=System.currentTimeMillis();
+            // t2 = end1 - start1;
 
-            long start2=System.currentTimeMillis();
-            f.randomAccess(dst);
-            long end2=System.currentTimeMillis();
-            t3 = end2 - start2;
+            // long start2=System.currentTimeMillis();
+            // f.randomAccess(dst);
+            // long end2=System.currentTimeMillis();
+            // t3 = end2 - start2;
 
             f.flag = 1;
             long start3=System.currentTimeMillis();
@@ -49,36 +49,36 @@ public class run {
             t4 = end3 - start3;
             
             long start4=System.currentTimeMillis();
-            f.readfromHDFS(src);
+            f.readfromHDFS(dst1);
             long end4=System.currentTimeMillis();
             t5 = end4 - start4;
 
             long start5=System.currentTimeMillis();
-            f.randomAccess(src);
+            f.randomAccess(dst1);
             long end5=System.currentTimeMillis();
             t6 = end5 - start5;
 
-            System.out.println("HDFS");
-            System.out.println("Total copy time "+(t1)+"ms");
-            System.out.println("Total read time "+(t2)+"ms");
-            System.out.println("Total random access time "+(t3)+"ms");
+            // System.out.println("HDFS");
+            // System.out.println("Total copy time "+(t1)+"ms");
+            // System.out.println("Total read time "+(t2)+"ms");
+            // System.out.println("Total random access time "+(t3)+"ms");
             System.out.println("Local");
             System.out.println("Total copy time "+(t4)+"ms");
             System.out.println("Total read time "+(t5)+"ms");
             System.out.println("Total random time "+(t6)+"ms");
-			File writename = new File(output); 
-			writename.createNewFile(); 
-			BufferedWriter out = new BufferedWriter(new FileWriter(writename));
-            out.write("HDFS\r\n"); 
-            out.write("Total copy time "+(t1)+"ms\r\n"); 
-            out.write("Total read time "+(t2)+"ms\r\n"); 
-            out.write("Total random access time "+(t3)+"ms\r\n"); 
-            out.write("Local\r\n"); 
-            out.write("Total copy time "+(t4)+"ms\r\n"); 
-            out.write("Total read time "+(t5)+"ms\r\n"); 
-            out.write("Total random time "+(t6)+"ms\r\n"); 
-            out.flush(); 
-            out.close();
+			// File writename = new File(output); 
+			// writename.createNewFile(); 
+			// BufferedWriter out = new BufferedWriter(new FileWriter(writename));
+            // out.write("HDFS\r\n"); 
+            // out.write("Total copy time "+(t1)+"ms\r\n"); 
+            // out.write("Total read time "+(t2)+"ms\r\n"); 
+            // out.write("Total random access time "+(t3)+"ms\r\n"); 
+            // out.write("Local\r\n"); 
+            // out.write("Total copy time "+(t4)+"ms\r\n"); 
+            // out.write("Total read time "+(t5)+"ms\r\n"); 
+            // out.write("Total random time "+(t6)+"ms\r\n"); 
+            // out.flush(); 
+            // out.close();
 
         }
         catch (IOException e) {
